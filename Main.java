061ec621e126a7b1b477bac9d75944b9d5bcedcb
loestenwolde <160247534+loestenwolde@users.noreply.github.com>
2024-03-25 15:24:10 +0100

    java hallo wereld

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+  public static void main(String[] args) {
+
+    /* 
+    Dit is een programma van Loes die samen met haar papa
+    een beetje met java aan het prutsen is.
+    Ja jezus heeeee
+    */
+
+
+    // Hier ga ik gewoon iets printen met tekst
+    System.out.println("Hello Loes");
+    System.out.println("hoe gaat het met je?");
+
+
+    // Hier ga ik met getallen prutsen
+    System.out.println( 15);
+    System.out.println(3+3);
+    System.out.println(5*5);
+    
+    
+    
+    
+}
